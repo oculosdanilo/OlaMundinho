261feadd87af6253cb30f51e85a1ff0055bc0709
oculosdanilo <oculosdanilo@gmail.com>
2024-06-01 17:10:25 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OlaMundinho;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.Write($"você gosta de sorvete?{Environment.NewLine}>>>");
+    var morri = Console.ReadLine();
+    var data = DateTime.Now;
+
+    Console.WriteLine($"foda-se, hoje é dia {data.Day} do {data.Month} de {data.Year}");
+    Console.ReadKey(true);
+  }
+}
